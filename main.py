6f5881669bfa24d7bf072e178ebd6ca827f640f5
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-07-25 21:27:04 +0800

    add hashing

--- main.py
+++ main.py
@@ -2,8 +2,25 @@ import os
 import hashlib
 import pickle
 
-def init():
-    return os.getcwd()
+def init_vcs():
 
-init = init()
-print(init)
+    """ Creates a hidden folder in the current working directory"""
+
+    os.makedirs(".vcs_storage", exist_ok=True)
+    print("VCS Initialized")
+
+def snapshot(get_directory):
+    snapshot_hash = hashlib.sha256()
+    snapshot_data = {"file" : {}}
+
+    for root, dirs, files in os.walk(get_directory):
+        for file in files:
+            if ".vcs_storage" in os.path.join(root, file):
+                continue
+
+            file_path = os.path.join(root, file)
+
+            with open(file_path, "rb") as f:
+                content = f.read()
+                snapshot_hash.update(content)
+                snapshot_data['file'][file_path] = content
